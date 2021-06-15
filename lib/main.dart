3b5cef2a1e1d791c8ba62a3840cddbf8b69987b8
h9419 <h9419@users.noreply.github.com>
2021-06-16 07:58:54 +0800

    misc

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> connectTV() async {
     try {
       setState(() async {
-        if (tv == null) tv = await SamsungSmartTV.discover();
+        tv = await SamsungSmartTV.discover();
         await tv.connect(() {
           setState(() {});
         });
